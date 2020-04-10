c1513c523d2025a3a61f475c3ded34f8f9086ea4
dymovcom <dymovcom@yandex.ru>
2020-04-10 17:26:02 +0500

    всё

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ const cardButton = document.querySelector("#card-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 
-cardButton.addEventListener('click', function (event) {
-    modal.classList.add("is-open");
-});
+cardButton.addEventListener('click', toggleModal);
 
-close.addEventListener("click", function (event) {
-    modal.classList.remove("is-open");
-});
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+new WOW().init();
